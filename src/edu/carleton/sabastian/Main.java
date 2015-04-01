7221fcf30a79055979226e22c0ab09911185c565
Sabastian Mugazambi <mugazambi@gmail.com>
2015-04-01 08:50:27 -0500

    Hello World

--- src/edu/carleton/sabastian/Main.java
+++ src/edu/carleton/sabastian/Main.java
@@ -3,6 +3,7 @@ package edu.carleton.sabastian;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println("hello, world! I am Sabastian Mugazambi and I love" +
+                " coding!");
     }
 }
