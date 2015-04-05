d5116094a4fd1a7899e17e0288f8b1382d6c2876
Sabastian Mugazambi <mugazambi@gmail.com>
2015-04-05 18:06:21 -0500

    EncodingHelperCharTest

---
+++ src/edu/carleton/sabastian/EncodingHelperCharTest.java
@@ -0,0 +1,41 @@
+package edu.carleton.sabastian;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sabastian on 4/5/15.
+ */
+public class EncodingHelperCharTest {
+
+    @Test
+    public void testGetCodePoint() throws Exception {
+
+    }
+
+    @Test
+    public void testSetCodePoint() throws Exception {
+
+    }
+
+    @Test
+    public void testToUtf8Bytes() throws Exception {
+
+    }
+
+    @Test
+    public void testToCodePointString() throws Exception {
+
+    }
+
+    @Test
+    public void testToUtf8String() throws Exception {
+
+    }
+
+    @Test
+    public void testGetCharacterName() throws Exception {
+
+    }
+}
